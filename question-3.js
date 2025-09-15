549c841bab3f9162be373a9e11f525fb0a2c9ac0
lalinyuuu <lullalin.yu@gmail.com>
2025-09-15 10:00:02 +0700

    feat: checkpoint 1-5

--- question-3.js
+++ question-3.js
@@ -1,13 +1,17 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
 Answer:
-
-
+O(n) > เพราะต้องวนลูปเช็คทุก student ทีละตัวจนกว่าจะเจอหรือไม่เจอ
+O(n) > cuz the function uses linear search and may need to check each student in the array.
 */
+
+
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
 Answer:
+O(n) > เพราะต้องวนลูปเช็คทุก product ทีละตัวเหมือนกัน
+O(n) > cuz the function also uses linear search and may need to check every product.
 
 
 */
@@ -15,6 +19,8 @@ Answer:
 /* 
 Which function is more efficient and why?
 Answer:
+both มีประสิทธิภาพเท่ากัน (O(n)) เนื่องจากต่างก็ใช้ Linear Search
+They are equally efficient (O(n)) since both rely on linear search.
 
 
 */
--- question-5.js
+++ question-5.js
@@ -1,6 +1,10 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
 Answer:
+O(n log n) >  cuz the built-in sort method in JavaScript typically uses a variation of 
+Quicksort, Timsort, or Merge sort, which all have average time complexity of O(n log n).
+O(n log n) > เนื่องจาก method sort() ใน JavaScript ใช้วิธีการเรียง แบบ
+Quicksort, Timsort หรือ Merge sort ซึ่งมีค่าเฉลี่ยเวลาในการทำงาน O(n log n)
 
 
 */
